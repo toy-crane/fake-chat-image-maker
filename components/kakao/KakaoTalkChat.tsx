dab74e8c921d19b6361afecddf60253ddf5a6f94
toy-crane <alwaysfun2183@gmail.com>
2025-06-06 16:20:01 +0900

    refactor: simplify ChatInput component and integrate into KakaoTalkChat
    - Removed state management and event handlers from ChatInput, making it a presentational component.
    - Updated KakaoTalkChat to include the simplified ChatInput, enhancing the chat interface structure.
    - Improved layout by replacing buttons with divs for a cleaner design in the input area.

--- components/kakao/KakaoTalkChat.tsx
+++ components/kakao/KakaoTalkChat.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from "./StatusBar";
 import { ChatHeader } from "./ChatHeader";
 import { MessageBubble } from "./MessageBubble";
 import { ImageMessage } from "./ImageMessage";
+import { ChatInput } from "./ChatInput";
 import { KakaoTalkChatProps, Message } from "./types";
 
 export function KakaoTalkChat({
@@ -88,6 +89,9 @@ export function KakaoTalkChat({
         {messages.map((message, index) => renderMessage(message, index))}
         <div ref={messagesEndRef} />
       </div>
+
+      {/* Input Area */}
+      <ChatInput />
 
       {/* Home Indicator */}
       <div className="bg-white pb-2 flex justify-center">
